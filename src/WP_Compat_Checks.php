31ffa02224ffbdba276f8cae2bc48d2e85eada0d
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2023-08-03 23:08:47 +0530

    add compat checker

--- src/WP_Compat_Checks.php
+++ src/WP_Compat_Checks.php
@@ -1,30 +1,73 @@
 <?php
 namespace Siddharth;
 
-class WP_Compat_Checks {
-	private $plugin_name = '';
-	private $min_php_version = '';
+class WP_Compat_Checker {
+	private $checklist = array();
 
-	public function set_plugin_name( $plugin_name = '' ) {
-		$this->plugin_name = $plugin_name;
+	private $messages = array();
+
+	public function __construct() {
+		$this->checklist = array(
+			'plugin_name'              => array(
+				'value'    => '',
+				'required' => true,
+			),
+			'php_min_required_version' => array(
+				'value'    => '',
+				'required' => false,
+			),
+			'php_max_required_version' => array(
+				'value'    => '',
+				'required' => false,
+			),
+		);
+	}
+
+	public function set_plugin_name( $value = '' ) {
+		$this->checklist['plugin_name']['value'] = $value;
 
 		return $this;
 	}
 
-	public function set_min_php_version( $min_php_version = '' ) {
-		$this->min_php_version = $min_php_version;
+	public function set_php_min_required_version( $value = '' ) {
+		$this->checklist['php_min_required_version']['value'] = $value;
 
 		return $this;
 	}
 
-	public function check() {
-		if ( empty( $this->plugin_name ) || empty( $this->min_php_version ) ) {
-			return null;
+	public function set_php_max_required_version( $value = '' ) {
+		$this->checklist['php_max_required_version']['value'] = $value;
+
+		return $this;
+	}
+
+	public function is_plugin_compatible() {
+		foreach ( $this->checklist as $item_name => $item_details ) {
+			if ( $item_details['required'] && empty( $item_details['value'] ) ) {
+				return false;
+			}
+
+			switch ( $item_name ) {
+				case 'php_min_required_version':
+					if ( version_compare( phpversion(), $item_details['value'], '<' ) ) {
+						$this->messages[] = sprintf( esc_html__( 'The minimum PHP version required is %s', $item_details['value'] ) );
+					}
+					break;
+
+				case 'php_max_required_version':
+					if ( version_compare( phpversion(), $item_details['value'], '>' ) ) {
+						$this->messages[] = sprintf( esc_html__( 'The maximum PHP version supported is %s', $item_details['value'] ) );
+					}
+					break;
+
+				default:
+					break;
+			}
 		}
 
-		if ( ! version_compare( phpversion(), $this->min_php_version, '>=' ) ) {
+		if ( ! empty( $this->messages ) ) {
 			add_action( 'admin_notices', array( $this, 'render_php_compat_error' ) );
-			return true;
+			return false;
 		}
 
 		return false;
@@ -33,15 +76,18 @@ class WP_Compat_Checks {
 	public function render_php_compat_error() {
 		?>
 		<div class="notice notice-error">
-			<p>
-				<?php
-				printf(
-					esc_html__( '%1$s requires PHP version %2$s or later. Please upgrade PHP or disable the plugin.' ),
-					$this->plugin_name,
-					$this->min_php_version
-				);
-				?>
-			</p>
+			<strong>
+				<?php printf( esc_html__( '%s error:' ), $this->checklist['plugin_name']['value'] ); ?>
+			</strong>
+			<?php if ( count( $this->messages ) > 1 ) : ?>
+				<ul>
+					<?php foreach ( $this->messages as $message ) : ?>
+						<li><?php echo esc_html( $message ); ?></li>
+					<?php endforeach; ?>
+				</ul>
+			<?php else: ?>
+				<?php echo esc_html( $message ); ?>
+			<?php endif; ?>
 		</div>
 		<?php
 	}
