d389fb89dc93f5cd2ac33efaf43de3ce9c81e753
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2023-09-02 00:56:51 +0530

    replace in vendor/composer as well

--- replace-namespace.sh
+++ replace-namespace.sh
@@ -3,6 +3,7 @@
 # Script directory and filename
 SCRIPT_DIR="$(dirname "$0")"
 SCRIPT_NAME="$(basename "$0")"
+COMPOSER_DIR="$(dirname "$(dirname "$SCRIPT_DIR")")/composer"
 
 # Check for the required argument
 if [ "$#" -ne 1 ]; then
@@ -14,6 +15,13 @@ NEW_NAMESPACE="$1"
 
 # Use find to get all files recursively from the script's directory, excluding the script itself
 find "$SCRIPT_DIR" -type f \( -name "*.php" -o -name "*.json" \) ! -name "$SCRIPT_NAME" | while read -r file; do
+	echo $file
+	# Use perl for the replacement in each file
+	perl -pi -e "s/WP_Compat_Tools/$NEW_NAMESPACE/g" "$file"
+done
+
+# Use find to get all files recursively in the vendor/composer directory
+find "$COMPOSER_DIR" -type f \( -name "*.php" -o -name "*.json" \) ! -name "$SCRIPT_NAME" | while read -r file; do
 	echo $file
 	# Use perl for the replacement in each file
 	perl -pi -e "s/WP_Compat_Tools/$NEW_NAMESPACE/g" "$file"
